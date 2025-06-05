a4a07c847f85299f5f47a755d064896a930077f0
José Malcher Jr. <contato@josemalcher.net>
2025-06-05 08:56:44 -0300

    49 Testando a primeira classe de domínio - Categoria

--- Secao-06-Estudo-de-caso-ImplementacaoJavacomSpringBooteJPA/Projeto01/src/main/java/net/josemalcher/modeloUml1/resources/CategoriaResource.java
+++ Secao-06-Estudo-de-caso-ImplementacaoJavacomSpringBooteJPA/Projeto01/src/main/java/net/josemalcher/modeloUml1/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package net.josemalcher.modeloUml1.resources;
 
+import net.josemalcher.modeloUml1.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar() {
-        return "REST OK";
+    public List<Categoria> listar() {
+
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritóorio");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
     }
 
 }
